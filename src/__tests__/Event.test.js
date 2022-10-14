4293aedee89c269a30bd996942d7ede94f696fa6
Andrey <shmalunandrey@gmail.com>
2022-10-14 11:25:22 -0400

    tests implementing

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
+
+describe('<Event /> component', () => {
+    let EventWrapper;
+
+    beforeAll(() => {
+        EventWrapper = shallow(<Event event={mockData[0]} />);
+    });
+
+    test('renders details outer container', () => {
+        const detailsContainer = EventWrapper.find('.details__outer-container');
+        expect(detailsContainer).toHaveLength(1);
+    });
+
+    test('renders details toggle', () => {
+        const detailsToggle = EventWrapper.find('.details__toggle');
+        expect(detailsToggle).toHaveLength(1);
+    });
+
+    test('renders details inner container', () => {
+        const details = EventWrapper.find('.details__inner-container');
+        expect(details).toHaveLength(1);
+    });
+
+    test('detail visibility is in sync with container visible class', () => {
+        const details = EventWrapper.find('.detail');
+        const visible = EventWrapper.find('.visible');
+        expect(!!details.length).toBe(!!(visible.length - 1));
+    });
+
+    test('button click toggles from invisible to visible', () => {
+        EventWrapper.setState({ isVisible: false })
+        const button = EventWrapper.find('.details__toggle')
+        button.simulate('click')
+        expect(EventWrapper.state("isVisible")).toEqual(true)
+    });
+
+    test('button click toggles from visible to invisible', () => {
+        EventWrapper.setState({ isVisible: true })
+        const button = EventWrapper.find('.details__toggle')
+        button.simulate('click')
+        expect(EventWrapper.state("isVisible")).toEqual(false)
+    });
+
+});
