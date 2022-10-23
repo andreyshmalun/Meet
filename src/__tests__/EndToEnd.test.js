47a3c3bbc30e422b64865659a95924d6449c91a3
Andrey <shmalunandrey@gmail.com>
2022-10-22 22:36:23 -0400

    fixed test error

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -3,12 +3,12 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
     let browser;
     let page;
+    jest.setTimeout(50000);
     beforeAll(async () => {
-        jest.setTimeout(30000);
         browser = await puppeteer.launch({
-            // headless: false,
-            // slowMo: 250, // slow down by 250ms
-            // ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+            headless: false,
+            slowMo: 100, // slow down by 250ms
+            ignoreDefaultArgs: ['--disable-extensions'], // ignores default setting that causes timeout errors
         });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
