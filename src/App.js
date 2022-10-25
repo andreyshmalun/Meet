1003531d0db9e743c170218b3e997b83107367a7
Andrey <shmalunandrey@gmail.com>
2022-10-25 19:08:03 -0400

    welcome page styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents.js';
-import { extractLocations, getEvents } from './api';
 import { OfflineAlert } from './Alert';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import WelcomeScreen from './WelcomeScreen';
 
 import './App.css';
 class App extends Component {
@@ -12,7 +13,8 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     selectedLocation: 'all',
-    offlineAlertText: ''
+    offlineAlertText: '',
+    showWelcomeScreen: undefined,
   };
 
   updateEvents = (location, eventCount) => {
@@ -39,13 +41,20 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -54,24 +63,28 @@ class App extends Component {
 
   render() {
     const { numberOfEvents } = this.state;
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
+
     return (
       <div className="App">
         <h1>Meet App</h1>
         <h4>Choose your nearest city</h4>
         <OfflineAlert text={this.state.offlineAlertText} />
-        <div className='navbar'>
-          <CitySearch
-            locations={this.state.locations}
-            updateEvents={this.updateEvents}
-          />
-          <NumberOfEvents
-            numberOfEvents={numberOfEvents}
-            updateEvents={this.updateEvents}
-          />
-        </div>
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents
+          numberOfEvents={numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+
         <div className='eventObject'>
           <EventList events={this.state.events} />
         </div>
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
